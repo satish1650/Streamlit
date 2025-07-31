b380601a7a85d9963aa03bb52414163460dbff57
Satish Kumar <satishsjce86@gmail.com>
2025-07-31 19:24:51 +0000

    streamlit app

---
+++ 02-text_elements/streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Example Full App:
+st.title("🚀 Sales Dashboard")
+
+st.markdown("### Summary")
+st.write("This app displays sales data and trends.")
+
+# You can use emojis and Unicode characters too:
+st.title("📊 Spend Analysis Tool")
