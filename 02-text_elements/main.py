41ba11213327349b4fada6cc748916d58f812a7e
Satish Kumar <satishsjce86@gmail.com>
2025-07-31 19:25:06 +0000

    main file

---
+++ 02-text_elements/main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+
+# Give your app a title
+st.title("My Streamlit App")
+st.title("Your title")
+st.markdown('# My Title')
+
+# Header
+st.header("Main header")
+st.header("Data Overview")
+
+# Subheader
+st.subheader("This is a subheader")
+st.subheader("Top Products This Month")
+
+# Preformatted text
+st.text("Some text")
+st.text("Hello Streamlit")
+st.text("Hello, World!\nThis is plain text.")
+
+
+# Markdown
+st.markdown("Markdown")
+st.markdown("This is markdown **text**")
+st.markdown("# Header1")
+st.markdown("## Header2")
+st.markdown("### Header3")
+st.markdown("#### Header4")
+st.markdown("##### Header5")
+st.markdown("###### Header6")
+st.markdown("####### Header7")
+st.markdown("# Hello, World!")
+st.markdown("**Bold text**, *italic text*, and [a link](https://streamlit.io)")
+st.markdown("__Bold text__")
+st.markdown("___Italic text___")
+st.markdown("***Italic text***")
+st.markdown("[streamlit documentation](https://docs.streamlit.io/)")
+
+# Caption
+st.caption("This is caption")
+
+# Code block
+st.code("""
+import pandas as pd
+pd.read_csv(my_csv_file)
+""")
+
+# LaTeX
+st.latex("x=2*2")
+st.latex("x=2^2, 4*5, 2^6")
+
+# Divider
+st.divider()
+st.text("Text above divider")
+st.divider()
+st.text("Text below divider")
+st.divider()
+
+# Write
+st.write("Some text")
+st.write("**This is bold**") # Interpreted as markdown
+st.write({"Name": "Satish",
+          "Age": 32}) # Displays dictionary
+st.write(42) # Displays number
