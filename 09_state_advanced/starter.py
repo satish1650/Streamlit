79c8a4b74ed00a4d171e62a95f09648daf60a787
Satish Kumar <satishsjce86@gmail.com>
2025-08-07 13:59:31 +0000

    session_state advanced

---
+++ 09_state_advanced/starter.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from datetime import datetime, timedelta
+
+st.title("Advanced State Management")
+
+# Store widget value in session state
+st.subheader("Store widget value in session state")
+
+# Initialize widget value with session state
+st.subheader("Initialize widget value with session state")
+
+# Callbacks
+st.subheader("Use callbacks")
+
+# Markdown for time range selection
+st.markdown("#### Select your time range")
+
+st.radio("Select a range", ["7 days", "28 days", "custom"], horizontal=True)
+
+st.radio("Select Gender", ["Male", "Female", "Other"])
+
+col1, col2, col3 = st.columns(3)
+col1.date_input("Start date")
+col2.date_input("End date")
